956bb2f6b9e80dc3bb3be2b7a83bdce2a1a6d7b5
Aditya Pandit <aditya.23cse@cambridge.edu.in>
2026-03-02 23:26:43 +0530

    Delete backend directory

--- backend/src/models/index.js
+++
@@ -1,8 +0,0 @@
-import "./user.model.js";
-import "./skill.model.js";
-import "./project.model.js";
-import "./application.model.js";
-import "./team.model.js";
-import "./task.model.js";
-import "./message.model.js";
-import "./notification.model.js";
